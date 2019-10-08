0110a75d378abcd7aa21ca794546ab55fbc479fb
aviskarkc10 <aviskarkc10@gmail.com>
2019-10-08 10:18:23 +0545

    Add commands for listing and converting exchange rates

--- main.go
+++ main.go
@@ -1,52 +1,69 @@
 package main
 
 import (
-  "fmt"
-  "os"
-  "strconv"
+	"fmt"
+	"os"
+	"strconv"
 
-  "github.com/urfave/cli"
-);
+	"github.com/urfave/cli"
+)
 
-const defaultCurrency = "usd";
-const defaultAmount = "1";
-
+const defaultCurrency = "usd"
+const defaultAmount = "1"
+const apiBaseUrl = "https://www.nrb.org.np/exportForexJSON.php"
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "exchange"
-  app.Usage = "Check the value of NPR against other currencies."
-  app.Action = exchange;
+	app := cli.NewApp()
+	app.Name = "exchange"
+	app.Usage = "Check the value of NPR against foreign currencies."
+	app.Commands = []cli.Command{
+		{
+			Name:    "convert",
+			Aliases: []string{"c"},
+			Usage:   "Convert foreign currency to NPR and vice versa.",
+			Action:  convert,
+		},
+		{
+			Name:    "list",
+			Aliases: []string{"l"},
+			Usage:   "Display the foreign exchange rates of all the available currencies.",
+			Action:  list,
+		},
+	}
 
-  app.Run(os.Args);
+	app.Run(os.Args)
 }
 
-func exchange(c *cli.Context) {
-  amount := c.Args().Get(0);
-  currency := c.Args().Get(1);
+func list(c *cli.Context) {
+	fmt.Printf("List")
+}
 
-  if (currency == "" && amount == "") {
-    fmt.Printf("Using default values '%s' and '%s' for currency and amount.\n", defaultCurrency, defaultAmount)
+func convert(c *cli.Context) {
+	amount := c.Args().Get(0)
+	currency := c.Args().Get(1)
 
-    amount = defaultAmount;
-    currency = defaultCurrency;
-  } else if (currency == "") {
-    fmt.Printf("Using default value '%s' for currency.\n", defaultCurrency)
+	if currency == "" && amount == "" {
+		fmt.Printf("Using default values '%s' and '%s' for currency and amount.\n", defaultCurrency, defaultAmount)
 
-    currency = defaultCurrency;
-  } else if (amount == "") {
-    fmt.Printf("Using default value '%s' for amount.\n", defaultAmount)
+		amount = defaultAmount
+		currency = defaultCurrency
+	} else if currency == "" {
+		fmt.Printf("Using default value '%s' for currency.\n", defaultCurrency)
 
-    amount = defaultAmount;
-  }
+		currency = defaultCurrency
+	} else if amount == "" {
+		fmt.Printf("Using default value '%s' for amount.\n", defaultAmount)
 
-  amt, err := strconv.Atoi(amount);
+		amount = defaultAmount
+	}
 
-  if(err != nil) {
-    fmt.Printf("Please input a valid number for amount.\n");
+	amt, err := strconv.Atoi(amount)
 
-    return;
-  }
+	if err != nil {
+		fmt.Printf("Please input a valid number for amount.\n")
 
-  fmt.Printf("You want to view value of '%d'.\n", amt);
+		return
+	}
+
+	fmt.Printf("You want to view value of '%d'.\n", amt)
 }
